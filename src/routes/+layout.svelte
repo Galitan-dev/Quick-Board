b12ebe11b10d56fc62cb6e0cf06a676a7b5d46fe
Galitan-dev <galitan.dev@gmail.com>
2023-08-02 10:13:54 +0200

    feat: header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,67 +1,13 @@
 <script>
-	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
-	import { webVitals } from '$lib/vitals';
+	import '../app.css'
 	import Header from './Header.svelte';
-	import './styles.css';
-
-	/** @type {import('./$types').LayoutServerData} */
-	export let data;
-
-	$: if (browser && data?.analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId: data.analyticsId
-		});
-	}
 </script>
 
-<div class="app">
-	<Header />
+<svelte:head>
+	<title>Quick-Board</title>
+	<meta name="description" content="Share, Cast, Connect: Quick-Board for Instant Visuals!" />
+</svelte:head>
 
-	<main>
-		<slot />
-	</main>
+<Header />
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
-</div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
+<slot />
