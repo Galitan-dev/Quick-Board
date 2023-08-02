4fc5b415c5d57af0772a9f43279a9f10836453ef
Galitan-dev <galitan.dev@gmail.com>
2023-08-02 11:56:21 +0200

    build: typescript, less and linters

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
-<script>
-	import '../app.css'
-	import Header from './Header.svelte';
+<script lang="ts">
+    import '../app.less';
+    import Header from './Header.svelte';
 </script>
 
 <svelte:head>
-	<title>Quick-Board</title>
-	<meta name="description" content="Share, Cast, Connect: Quick-Board for Instant Visuals!" />
+    <title>Quick-Board</title>
+    <meta name="description" content="Share, Cast, Connect: Quick-Board for Instant Visuals!" />
 </svelte:head>
 
 <Header />
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,16 +1,16 @@
-<script>
-    import { PUBLIC_GITHUB_REPOSITORY } from "$env/static/public";
-    import logo from "$lib/images/logo.svg";
-    import github from "$lib/images/github.png";
+<script lang="ts">
+    import { PUBLIC_GITHUB_REPOSITORY } from '$env/static/public';
+    import logo from '$lib/images/logo.svg';
+    import github from '$lib/images/github.png';
 </script>
 
 <header>
-    <img src={logo} alt="Quick-Board logo">
+    <img src={logo} alt="Quick-Board logo" />
     <h1>Quick-Board</h1>
-    <a target="_blank" href={PUBLIC_GITHUB_REPOSITORY}><img src={github} alt="github logo"></a>
+    <a target="_blank" href={PUBLIC_GITHUB_REPOSITORY}><img src={github} alt="github logo" /></a>
 </header>
 
-<style>
+<style lang="less">
     header {
         height: 50px;
         display: flex;
